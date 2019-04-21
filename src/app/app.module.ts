afc3925152c5c501d0218f13bde153eb8eec68aa
Austin Fennacy <austinfennacy@gmail.com>
2019-04-20 17:57:46 -0700

    added graph component w mock image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphComponentComponent } from './graph-component/graph-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphComponentComponent
   ],
   imports: [
     BrowserModule,
